69761f95dfb51cce05d1ed10a96db63964e1870f
gregfromstl <gregfromstl@gmail.com>
2024-07-07 12:41:06 -0700

    biome

--- init.lua
+++ init.lua
@@ -493,6 +493,40 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
+        biome = {
+          {
+            cmd = { 'biome', 'lsp-proxy' },
+            filetypes = {
+              'javascript',
+              'javascriptreact',
+              'json',
+              'jsonc',
+              'typescript',
+              'typescript.tsx',
+              'typescriptreact',
+              'astro',
+              'svelte',
+              'vue',
+              'css',
+            },
+            root_dir = require('lspconfig.util').root_pattern('biome.json', 'biome.jsonc'),
+            single_file_support = false,
+          },
+          docs = {
+            description = [[
+https://biomejs.dev
+
+Toolchain of the web. [Successor of Rome](https://biomejs.dev/blog/annoucing-biome).
+
+```sh
+npm install [-g] @biomejs/biome
+```
+]],
+            default_config = {
+              root_dir = [[root_pattern('biome.json', 'biome.jsonc')]],
+            },
+          },
+        },
 
         lua_ls = {
           -- cmd = {...},
